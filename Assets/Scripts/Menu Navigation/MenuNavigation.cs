1d65778e489d13d1ae68280ff39ee63fc113dd67
MariaL23 <91759715+MariaL23@users.noreply.github.com>
2023-09-18 11:45:41 +0200

    UI Added

--- Assets/Scripts/Menu Navigation/MenuNavigation.cs
+++ Assets/Scripts/Menu Navigation/MenuNavigation.cs
@@ -27,6 +27,34 @@ public class MenuNavigation : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
+    public void LoadMainPage(string sceneName)
+    {
+        Debug.Log("Loading Scene...");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+    public void LoadSetupPage(string sceneName)
+    {
+        Debug.Log("Loading Scene...");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+    public void LoadPlacementScene(string sceneName)
+    {
+        Debug.Log("Loading Scene...");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+    }
+
+    public void LoadExercisePage(string sceneName)
+    {
+        Debug.Log("Loading Scene...");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(5);
+    }
+    public void LoadSummary(string sceneName)
+    {
+        Debug.Log("Loading Scene...");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+    }
+
+
     public void DismissUI()
     {
         GameObject.SetActive(false);
